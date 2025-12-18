b232e13748b0159d47bf9dd1727d01d69e246a56
sushantsubedi35 <sushantsubedi352@gmail.com>
2025-12-18 10:58:10 +0545

    update

---
+++ Day_13/Guessnum.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+ class Guessnum {
+    static int hiddenNumber = 14;
+    static int userInput;
+    static Scanner sc = new Scanner(System.in);
+
+    Guessnum(int hidNum, int userInput){
+        hiddenNumber = hidNum;
+        this.userInput = userInput;
+    }
+    
+    static void getNumber(){
+        System.out.print("Enter the number you are guessing : ");
+        userInput = sc.nextInt();
+     }
+
+     public static void main(String [] args) {
+        boolean running = true;
+
+        System.out.println(" Welcome to The Number Guessing Game. ");
+        
+        System.out.println("Guess a Number between 1 to 100");
+
+        getNumber();
+
+        while(running){
+            if(hiddenNumber < userInput){
+                System.out.println("Your Guessed number is high");
+                getNumber();
+
+            } else if(hiddenNumber > userInput){
+                System.out.println("Your Guessed number is Low");
+                getNumber();
+
+            } else if(hiddenNumber == userInput){
+                System.out.println("Your Guessed number is correct");
+                running = false;
+        
+            }
+            else {
+                System.out.println("Invalid Input");
+                getNumber();
+            }
+            
+        }
+     }
+ }
+
+ 
+    
+ 
